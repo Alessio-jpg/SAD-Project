7c837126a723aee8157846fccff4511f461373c3
vincpasc <vincen.pascarella@studenti.unina.it>
2022-06-24 15:54:03 +0200

    PS-7
    Start Page

---
+++ Client/src/Pages/ScoreBoard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ScoreBoard() {
+  return <div>THIS IS THE SCOREBOARD PAGE</div>;
+}
+
+export default ScoreBoard;
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Pages/App';
 import reportWebVitals from './reportWebVitals';
+import Routing from './Routing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routing/>
   </React.StrictMode>
 );
 
