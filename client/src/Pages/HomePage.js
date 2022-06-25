71eeaeba2f51644a48788be55d6560127cf8bf6b
Alessio-jpg <alessi.paesano@studenti.unina.it>
2022-06-25 18:31:50 +0200

    Revert "PS-7"
    This reverts commit 683206e4fc983b127c4b8b1ea799aed4c47f37d5.

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import './HomePage.css';
 
 function HomePage() {
   let navigate = useNavigate();
   return (
     <div>
       THIS IS THE HOMEPAGE PAGE!
-      <button className="button-play" onClick={() => {
-        navigate("/App");}}>
-      Start Drawing</button>
+      <button
+        onClick={() => {
+          navigate("/App");
+        }}
+      >
+        {" "}
+        Start Drawing
+      </button>
       <button
         onClick={() => {
           navigate("/ScoreBoard");
