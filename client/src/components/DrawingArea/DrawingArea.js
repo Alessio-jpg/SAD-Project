d422389d73c491397cceac426a3b05b74ced57c6
RaimondoR <reggioraimondo@gmail.com>
2022-06-25 17:12:35 +0200

    PS-5

--- client/src/components/DrawingArea/DrawingArea.js
+++ client/src/components/DrawingArea/DrawingArea.js
@@ -1,6 +1,6 @@
 import { React } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEraser } from '@fortawesome/free-solid-svg-icons'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
@@ -71,6 +71,9 @@ const DrawingArea = ({onClearLines, clearLines}) => {
                     onMouseDown={handleMouseDown}
                     onMousemove={handleMouseMove}
                     onMouseup={handleMouseUp}
+                    onTouchStart={handleMouseDown}
+                    onTouchMove={handleMouseMove}
+                    onTouchEnd={handleMouseUp}
                     className="canvas-stage"
                 >
                     <Layer>
