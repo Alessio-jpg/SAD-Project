683206e4fc983b127c4b8b1ea799aed4c47f37d5
vincpasc <vincen.pascarella@studenti.unina.it>
2022-06-25 18:15:19 +0200

    PS-7
    Start Button

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import './HomePage.css';
 
 function HomePage() {
   let navigate = useNavigate();
   return (
     <div>
       THIS IS THE HOMEPAGE PAGE!
-      <button
-        onClick={() => {
-          navigate("/App");
-        }}
-      >
-        {" "}
-        Start Drawing
-      </button>
+      <button className="button-play" onClick={() => {
+        navigate("/App");}}>
+      Start Drawing</button>
       <button
         onClick={() => {
           navigate("/ScoreBoard");
