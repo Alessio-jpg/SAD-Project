424933dbc6a70b8ee800f69d93b23e382ec7ac77
RaimondoR <reggioraimondo@gmail.com>
2022-06-26 19:11:41 +0200

    PS-13
    Demo Login Page without CSS

---
+++ client/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../../firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import './HomePage.css';
+import imm from '../../assets/home-image.png'
+
+function HomePage() {
+  let navigate = useNavigate();
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState("");
+  const fetchUserName = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+      setName(data.name);
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/");
+    fetchUserName();
+  }, [user, loading]);
+  return (
+    <div className="home-area">
+      <div className="home-container">
+        <div className="image">
+          <img src={imm}></img>
+        </div>
+        <div className="username">
+          <p> {name}</p>
+        </div>
+        <div className="first-button">
+          <button className="button-play" onClick={() => {
+          navigate("/App");}}>
+          Start Drawing
+          </button>
+        </div>
+        <div className="second-button">
+          <button className="button-score" onClick={() => {
+          navigate("/ScoreBoard");}}>
+          View ScoreBoard
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HomePage;
--- client/src/components/Routing.js
+++ client/src/components/Routing.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import HomePage from "../Pages/HomePage";
-import ScoreBoard from "../Pages/ScoreBoard";
+import HomePage from "../Pages/HomePage/HomePage";
+import ScoreBoard from "../Pages/ScoreBoard/ScoreBoard";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 import App from "../main/App";
 
 function Routing() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/Register" element={<Register />} />
         <Route path="/App" element={<App />} />
         <Route path="/ScoreBoard" element={<ScoreBoard />} />
       </Routes>
