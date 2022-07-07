f18d20c2da15f3a9568212458f99423676248a90
RaimondoR <reggioraimondo@gmail.com>
2022-07-07 17:17:28 +0200

    PS-14

--- client/src/Pages/Register/Register.js
+++ client/src/Pages/Register/Register.js
@@ -1,28 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from '../../assets/home-image.png';
-import {
-  auth,
-  registerWithEmailAndPassword,
-} from "../../firebase";
+import Axios from "axios";
 import "./Register.css";
 
 function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [user, loading] = useAuthState(auth);
-  const navigate = useNavigate();
-  const register = () => {
-    if (!name) alert("Please enter name");
-    registerWithEmailAndPassword(name, email, password);
-  };
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordeReg, setPasswordReg] = useState("");
 
-  useEffect(() => {
-    if (loading) return;
-    if (user) navigate("/HomePage");
-  }, [user, loading]);
+  Axios.defaults.withCredentials = true;
+
+  const register = () => {
+    Axios.post("http://localhost:4000/register", {
+        username: usernameReg,
+        password: passwordeReg,
+    }).then((response) => {
+        console.log(response);
+    });
+  };
   
   return (
     <div className="register">
@@ -33,22 +28,15 @@ function Register() {
         <input
           type="text"
           className="register__textBox"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={usernameReg}
+          onChange={(e) => setUsernameReg(e.target.value)}
           placeholder="Full Name"
-        />
-        <input
-          type="text"
-          className="register__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
         />
         <input
           type="password"
           className="register__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={passwordeReg}
+          onChange={(e) => setPasswordReg(e.target.value)}
           placeholder="Password"
         />
         <button className="register__btn" onClick={register}>
